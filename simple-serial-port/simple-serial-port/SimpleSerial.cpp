adc2aa41b4bbc1578c39a5e90775369839949077
David Michalik <dmicha16@student.aau.dk>
2018-01-24 14:10:12 +0100

    Initial commit

---
+++ simple-serial-port/simple-serial-port/SimpleSerial.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SimpleSerial.h"
+
+
+SimpleSerial::SimpleSerial()
+{
+}
+
+
+SimpleSerial::~SimpleSerial()
+{
+}
---
+++ simple-serial-port/simple-serial-port/SimpleSerial.h
@@ -0,0 +1,12 @@
+#pragma once
+class SimpleSerial
+{
+public:
+	SimpleSerial();
+
+
+
+
+	~SimpleSerial();
+};
+
