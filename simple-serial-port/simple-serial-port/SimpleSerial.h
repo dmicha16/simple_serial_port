bb73cf2e822af387dc94f0a812cea44e7ae8be2e
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-12-23 23:08:31 +0100

    Removed hard tabs -> bad practice

--- simple-serial-port/simple-serial-port/SimpleSerial.h
+++ simple-serial-port/simple-serial-port/SimpleSerial.h
@@ -15,24 +15,24 @@ class SimpleSerial
 {
 
 private:
-	HANDLE io_handler_;
-	COMSTAT status_;
-	DWORD errors_;
+    HANDLE io_handler_;
+    COMSTAT status_;
+    DWORD errors_;
 
-	std::string syntax_name_;
-	char front_delimiter_;
-	char end_delimiter_;
+    std::string syntax_name_;
+    char front_delimiter_;
+    char end_delimiter_;
 
-	void CustomSyntax(std::string syntax_type);	
+    void CustomSyntax(std::string syntax_type);	
 
 public:
-	SimpleSerial(char* com_port, DWORD COM_BAUD_RATE);
-	void init(char* com_port, DWORD COM_BAUD_RATE);
+    SimpleSerial(char* com_port, DWORD COM_BAUD_RATE);
+    void init(char* com_port, DWORD COM_BAUD_RATE);
 
-	std::string ReadSerialPort(int reply_wait_time, std::string syntax_type);	
-	bool WriteSerialPort(char *data_sent);
-	bool CloseSerialPort();
-	~SimpleSerial();
-	bool connected_;
+    std::string ReadSerialPort(int reply_wait_time, std::string syntax_type);	
+    bool WriteSerialPort(char *data_sent);
+    bool CloseSerialPort();
+    ~SimpleSerial();
+    bool connected_;
 };
 
